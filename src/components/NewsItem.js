e5b829b11a0b12bb893697aabbfec54c6c6cfdd5
Sharyu <sharayumarwadi11@gmail.com>
2022-03-02 14:17:57 +0530

    Added infinite scroll

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,14 +7,14 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
           <div className="card">
-          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%",zIndex:'1'}}>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%",zIndex:'1'}}>
             {source}
           </span>
             <img src={!ImgUrl?"https://images.moneycontrol.com/static-mcnews/2022/02/nifty_sensex_stocks-770x433.jpg":ImgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{discription}</p>
-                <p className="card-text"><small className="text-muted">BY {!author?"Anonymous":author} on {new Date(date).toGMTString()}</small></p>
+                <p className="card-text"><small className="text-danger">BY {!author?"Anonymous":author} on {new Date(date).toGMTString()}</small></p>
                 <a href={NewsUrl} rel="noreferrer"  target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
             </div>
